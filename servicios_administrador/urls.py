564454c15813a84a0fad0494e26c669d66db17ce
Daniel <dan.cabreraa@duocuc.cl>
2025-05-22 21:34:43 -0400

    Actualiza estilos, URLs, vistas; elimina plantillas obsoletas y añade nuevas

--- servicios_administrador/urls.py
+++ servicios_administrador/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import GestionDisponibilidadView, ListaDisponibilidadView, disponibilidad_view
+from .views import disponibilidad_view, confirmacion_adm_view
 
 app_name = 'servicios_administrador'
 
 urlpatterns = [
- path('nueva-disponibilidad/', GestionDisponibilidadView.as_view(), name='nueva_disponibilidad'),
- path('disponibilidad/', disponibilidad_view, name='disponibilidad_view'),
+  path('disponibilidad/', disponibilidad_view, name='disponibilidad_view'),
+  path('confirmacion/', confirmacion_adm_view, name='confirmacion_adm_view'),
 
 ]
--- servicios_administrador/views.py
+++ servicios_administrador/views.py
@@ -1,26 +1,23 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.shortcuts import render, redirect
 from .models import Disponibilidad
-from .forms import DisponibilidadForm
 
-# Vista para GESTIONAR disponibilidades (administradores)
-class GestionDisponibilidadView(CreateView):
-    model = Disponibilidad
-    form_class = DisponibilidadForm
-    template_name = 'servicios/gestion_disponibilidad.html'
-    success_url = '/servicios/disponibilidad/'
 
-# Vista para LISTAR disponibilidades (usuarios)
-class ListaDisponibilidadView(ListView):
-    model = Disponibilidad
-    template_name = 'servicios/lista_disponibilidad.html'
-    context_object_name = 'disponibilidades'
-
-    def get_queryset(self):
-        fecha = self.request.GET.get('fecha')
-        if fecha:
-            return Disponibilidad.objects.filter(dia=fecha)
-        return Disponibilidad.objects.all()
-    
+# Vista para CREAR disponibilidades (ADMINISTRADOR)  
 def disponibilidad_view(request):
-    return render(request, 'mi_web/Disponibilidad.html')
+    if request.method == 'POST':
+        print("DATOS RECIBIDOS:", request.POST) #Imprime los datos recibidos en la consola para depuración.
+        #Guardar los datos del formulario en la base de datos
+        Disponibilidad.objects.create(
+            administrador=request.POST['administrador'],
+            dia=request.POST['dia'],
+            hora_inicio=request.POST['hora_inicio'],
+            hora_fin=request.POST['hora_fin'],
+            servicio=request.POST['servicio'],
+            disponible=True # esta linea guarda el valor de la casilla de verificación y lo convierte en un booleano, un booleano es un tipo de dato que solo puede tener dos valores: verdadero o falso.
+            )             
+        return redirect('mi_web/Confirmacion.html') #Redirect es una función que redirige al usuario a otra URL después de que se haya procesado el formulario. En este caso, redirige a la plantilla 'Confirmacion.html' después de guardar la disponibilidad en la base de datos.
+    return render(request, 'mi_web/Disponibilidad.html')#En caso de que la solicitud no sea un POST, se renderiza la plantilla 'Disponibilidad.html' para mostrar el formulario al usuario.
+
+# Vista para confirmar datos guardados al ofrecer disponibilidad (ADMINISTRADOR)
+def confirmacion_adm_view(request):
+    return render(request, 'mi_web/Confirmacion.html')
