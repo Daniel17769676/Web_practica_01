dad28fe281e18c1728373a681fb91bb724cc492b
Daniel <dan.cabreraa@duocuc.cl>
2025-06-12 17:01:48 -0400

    Update page designs

--- reservas/views.py
+++ reservas/views.py
@@ -40,38 +40,15 @@ def get_dias_por_servicio_view(request):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
 
-
-
         
 # Vista para procesar la reserva (POST)
 def procesar_reserva_view(request):
-    if request.method == 'POST':
-        #obtener los datos del formulario
-        servicio = request.POST.get('servicio')
-        dia = request.POST.get('dia')
-        nombre = request.POST.get('nombre')
-        telefono = request.POST.get('telefono')
-        email = request.POST.get('email')
-        observaciones = request.POST.get('observaciones') 
-
-        # Guardar los datos del formulario en la base de datos  
-        reserva = Reserva(
-            servicio=servicio,
-            dia=dia,
-            horario=horario,
-            nombre_cliente=nombre,
-            telefono=telefono,
-            email=email,
-            observaciones=observaciones
-        )
-        reserva.save()
-
-        return redirect('reservas:reserva_exito')  # Redirige a la plantilla de confirmación
+    #Agregar la lógica para procesar la reserva (PENDIENTE)
     return render(request, 'reservas/reservar.html') 
 
 
+# Renderiza la plantilla de confirmación de reserva exitosa
 def reserva_exito_view(request):
-    # Renderiza la plantilla de confirmación de reserva exitosa
     return render(request, 'reservas/reserva_exito.html')
 
 
